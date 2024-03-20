0a77fee87d1b06e366344cbbc7336e44322c737a
Jorge Peguero <doiany.1412@gmail.com>
2024-03-20 11:16:12 -0400

    Creation of encryptor files

---
+++ script.js
@@ -0,0 +1,16 @@
+/* let a = "ai";
+let e = "enter";
+let i = "imes";
+let o = "ober";
+let u = "ufat"; */
+
+function desencriptar() {
+  alert("click desde el boton");
+}
+
+function encriptarMensaje() {
+  let mensajePrincipal = document.getElementById("mensajeDeEntrada").value;
+  document.getElementById("primer-panel").style.display = "none";
+  console.log(mensajePrincipal);
+  return;
+}
