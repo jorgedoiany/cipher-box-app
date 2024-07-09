e7c001cfa00a79eecea9e6217aadad57bb344e5b
Jorge Peguero <doiany.1412@gmail.com>
2024-07-09 12:20:48 -0400

    Final code of the application

---
+++ README.md
@@ -0,0 +1,42 @@
+# CipherBox
+
+**CipherBox** is an application designed to encrypt and decrypt text in a straightforward manner. It allows users to enter text into a specific field, where they can easily encrypt it. Additionally, it provides the ability to copy the encrypted text and paste it back to decrypt it. It is useful for anyone needing to protect the privacy of their text through encryption and then retrieve it through the reverse decryption process.
+
+## Screenshots
+
+![App Screenshot](./recursos/start-img.png)
+![App Screenshot](./recursos/encripted-img.png)
+![App Screenshot](./recursos/decripted-img.png)
+
+## Tech Stack
+
+**Client:** HTML, CSS, JavaScript
+
+## Prerequisites
+
+- **Visual Studio Code (VSCode)**: You can download it from [Visual Studio Code](https://code.visualstudio.com/).
+
+## Installation Steps
+
+### Download the project:
+
+1. Download the project from the repository or directly as a ZIP file.
+
+### Open the project in Visual Studio Code:
+
+1. Unzip the ZIP file (if you downloaded the project as a ZIP).
+2. Open Visual Studio Code.
+3. Select `File` > `Open Folder` and navigate to the downloaded project folder.
+
+### Run the application:
+
+1. If you're only using HTML, CSS, and JavaScript, simply open the `index.html` file in your preferred web browser.
+2. Alternatively, you can use the Live Server extension in VSCode to run the application locally:
+   - Install the **Live Server** extension from the VSCode marketplace.
+   - Right-click the `index.html` file and select `Open with Live Server`.
+
+That's it! You should now be able to see and use the **CipherBox** application in your browser.
+
+## Author
+
+- [@jorgedoiany](https://github.com/jorgedoiany)
--- script.js
+++ script.js
@@ -1,16 +1,54 @@
-/* let a = "ai";
-let e = "enter";
-let i = "imes";
-let o = "ober";
-let u = "ufat"; */
+const encriptarMensaje = () => {
+  let mensaje = document.getElementById("mensajeDeEntrada").value;
+  if (!validarEntrada(mensaje)) {
+    alert(
+      "El mensaje contiene caracteres inválidos. Por favor, use solo letras minúsculas y sin acentos."
+    );
+    return;
+  }
+  let mensajeEncriptado = mensaje
+    .replace(/e/g, "enter")
+    .replace(/i/g, "imes")
+    .replace(/a/g, "ai")
+    .replace(/o/g, "ober")
+    .replace(/u/g, "ufat");
 
-function desencriptar() {
-  alert("click desde el boton");
-}
+  mostrarMensaje(mensajeEncriptado);
+};
 
-function encriptarMensaje() {
-  let mensajePrincipal = document.getElementById("mensajeDeEntrada").value;
+const desencriptarMensaje = () => {
+  let mensaje = document.getElementById("mensajeDeEntrada").value;
+  if (!validarEntrada(mensaje)) {
+    alert(
+      "El mensaje contiene caracteres inválidos. Por favor, use solo letras minúsculas y sin acentos."
+    );
+    return;
+  }
+  let mensajeDesencriptado = mensaje
+    .replace(/enter/g, "e")
+    .replace(/imes/g, "i")
+    .replace(/ai/g, "a")
+    .replace(/ober/g, "o")
+    .replace(/ufat/g, "u");
+
+  mostrarMensaje(mensajeDesencriptado);
+};
+
+const validarEntrada = (texto) => {
+  const regex = /^[a-z\s]*$/;
+  return regex.test(texto);
+};
+
+const mostrarMensaje = (mensaje) => {
   document.getElementById("primer-panel").style.display = "none";
-  console.log(mensajePrincipal);
-  return;
-}
+  const outputPanel = document.getElementById("output-panel");
+  outputPanel.style.display = "block";
+  document.getElementById("mensajeDeSalida").value = mensaje;
+};
+
+const copiarTexto = () => {
+  const mensajeSalida = document.getElementById("mensajeDeSalida");
+  mensajeSalida.select();
+  document.execCommand("copy");
+  alert("Texto copiado al portapapeles");
+};
